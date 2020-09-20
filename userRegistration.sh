d25a0b548136bef6154c9197cd3ba9d50c4dce31
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-20 12:26:00 +0530

    Added : First Name Functionality

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "Welcome to the User Registration Program"
-
+read -p "Enter First Name : " firstName
+pattern="^[[:upper:]]{1}[a-zA-Z0-9]{2,}$"
+if [[ $firstName =~ $pattern ]]
+        then
+                echo "First Name : $firstName is Valid"
+        else
+                echo "First Name : $firstName is an Invalid"
+        fi

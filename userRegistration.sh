52fc4fdf92fce745f47d3cdc0dcffcedafa077a2
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-20 12:31:57 +0530

    Added : Email Functionality

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,6 +3,7 @@ echo "Welcome to the User Registration Program"
 read -p "Enter First Name : " firstName
 read -p "Enter Last Name : " lastName
 pattern="^[[:upper:]]{1}[a-zA-Z0-9]{2,}$"
+emailPattern="^[[:lower:]]{3}[a-zA-Z0-9]*([-+_.]?[a-zA-Z0-9])*@[a-z]{2,}.[a-z]{2,3}([.][a-z]{2})*$"
 if [[ $firstName =~ $pattern ]]
         then
                 echo "First Name : $firstName is Valid"
@@ -14,5 +15,11 @@ if [[ $lastName =~ $pattern ]]
                 echo "Last Name : $lastName is Valid"
         else
                 echo "Last Name : $lastName is an Invalid"
+        fi
+if [[ $email =~ $emailPattern ]]
+        then
+                echo "$email : It's Valid Email"
+        else
+                echo "$email : It's an Invalid Email"
         fi
 

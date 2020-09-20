d003a816ad7d1d84ac466fdd7214cfa5e2fadfb6
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-20 12:28:12 +0530

    Added : Last Name Functionality

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Welcome to the User Registration Program"
 read -p "Enter First Name : " firstName
+read -p "Enter Last Name : " lastName
 pattern="^[[:upper:]]{1}[a-zA-Z0-9]{2,}$"
 if [[ $firstName =~ $pattern ]]
         then
@@ -8,3 +9,10 @@ if [[ $firstName =~ $pattern ]]
         else
                 echo "First Name : $firstName is an Invalid"
         fi
+if [[ $lastName =~ $pattern ]]
+        then
+                echo "Last Name : $lastName is Valid"
+        else
+                echo "Last Name : $lastName is an Invalid"
+        fi
+
